b6213200f9c37ac843abd0bd2d78172452fdbdb6
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-04-20 20:05:05 +0700

    BigClass-Membuat class baru bernama Spouse
    Ditambahkan class baru bernama Spouse, bertujuan untuk mengefektifkan atribut dari class employe sehingga mudah di baca dan di control. Class Year Meng Extend Class empolyee sebagai Parent

---
+++ TaxCalculator/src/lib/Spous.java
@@ -0,0 +1,11 @@
+package lib;
+
+public class Spous {
+    private String spouseName;
+    private String spouseIdNumber;
+
+    public Spous(String spouseName, String spouseIdNumber) {
+        this.spouseName = spouseName;
+        this.spouseIdNumber = spouseIdNumber;
+    }
+}
