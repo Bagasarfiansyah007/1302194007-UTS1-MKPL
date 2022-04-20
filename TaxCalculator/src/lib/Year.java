b52c99559fb2c35bfe1f4250672460b6f7246160
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-04-20 21:36:25 +0700

    LongParameter- Merubah parameter konstruktor
    Merubah parameter dan menambahkan variabel salary spouse

--- TaxCalculator/src/lib/Year.java
+++ TaxCalculator/src/lib/Year.java
@@ -7,13 +7,25 @@ public class Year extends Employee{
     private int monthJoined;
     private int dayJoined;
     private int monthWorkingInYear;
+    Salary salary;
+    Spous spouse;
 
-    public Year(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender, int yearJoined1, int monthJoined1, int dayJoined1, int monthWorkingInYear) {
-        super(employeeId, firstName, lastName, idNumber, address, yearJoined, monthJoined, dayJoined, isForeigner, gender);
-        this.yearJoined = yearJoined1;
-        this.monthJoined = monthJoined1;
-        this.dayJoined = dayJoined1;
-        this.monthWorkingInYear = monthWorkingInYear;
+    public Year(Employee employee, Salary salary, Spous spouse, int yearJoined, int monthJoined, int dayJoined)
+    {
+        super(
+                employee.getEmployeeId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getIdNumber(),
+                employee.getAddress(),
+                employee.isForeigner(),
+                employee.getGender()
+        );
+        this.salary = salary;
+        this.spouse = spouse;
+        this.yearJoined = yearJoined;
+        this.monthJoined = monthJoined;
+        this.dayJoined = dayJoined;
     }
 
     public int getAnnualIncomeTax() {
