485a3cb23101ccf2d7ff3c1a420da10969c7cde3
Bagasarfiansyah007 <bagasarfiansyah007@gmail.com>
2022-04-20 20:54:07 +0700

    LongParamaterList - Mengubah parameter constructor
    Dalam commit ini  saya merubah parameter constructor.
    
    Daftar Perubahan :
    - Mengganti parameter
    - Memindahkan setter class year dan salary

--- TaxCalculator/src/lib/Year.java
+++ TaxCalculator/src/lib/Year.java
@@ -1,5 +1,7 @@
 package lib;
 
+import java.time.LocalDate;
+
 public class Year extends Employee{
     private int yearJoined;
     private int monthJoined;
@@ -12,5 +14,19 @@ public class Year extends Employee{
         this.monthJoined = monthJoined1;
         this.dayJoined = dayJoined1;
         this.monthWorkingInYear = monthWorkingInYear;
+    }
+
+    public int getAnnualIncomeTax() {
+
+        //Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+        LocalDate date = LocalDate.now();
+
+        if (date.getYear() == yearJoined) {
+            monthWorkingInYear = date.getMonthValue() - monthJoined;
+        }else {
+            monthWorkingInYear = 12;
+        }
+
+        return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
     }
 }
